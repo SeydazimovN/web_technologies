ceb34cfad8b517d7cd7380c6550a12e7e3664bc0
SeydazimovN <nurbol.seydazimov@gmail.com>
2017-05-08 20:49:53 +0600

    final project

---
+++ final project/login.js
@@ -0,0 +1,33 @@
+var space = {
+  get: function(key){
+    return JSON.parse(localStorage.getItem(key));
+  },
+
+  set: function(key, val){
+    var tmp = JSON.stringify(val);
+    localStorage.setItem(key, tmp);
+  },
+
+  remove: function(key){
+    localStorage.removeItem(key);
+  },
+
+  clear: function(){
+    localStorage.clear();
+  }
+};
+
+$(document).ready(function() {
+
+  $('.login-user').on('click', function() {
+    var accountName = $('.login-input').val();
+    console.log("account " + accountName);
+
+    $('.login-input').val('');
+
+    space.set('myAccount', accountName);
+
+    // alert('all things are good!');
+    window.location='file:///home/seydazimovnurbol/Desktop/final%20project/messenger.html';
+  })
+})
